7ee33f3ec47d7a28b5b4c74dce3b9db5abaf7f91
olegator <lelik.belokonevvv@gmail.com>
2022-05-13 10:42:41 +0400

    feat(login): create page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,9 +3,11 @@ import React, { useCallback, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import SubmitButton from '@components/forms/submit-button';
+import combineCss from '@helpers/combine-css';
 import type { SSRComponent } from '@interfaces/ssr-component';
 import type { StoreProps } from './index.stores';
 import validationSchema from './validation-schema';
+import styles from './styles.module.scss';
 
 type Props = StoreProps;
 
@@ -18,8 +20,8 @@ export interface ILoginForm {
  * Login page
  */
 const Login: SSRComponent<Props> = ({ authStore: { error, signIn, isLoading } }) => {
-  const { t } = useTranslation(['login-page', 'forms']);
-  const [initialValues] = useState<ILoginForm>({ login: 'test@test.com', password: '123456789' });
+  const { t } = useTranslation(['login-page', 'forms', 'translation']);
+  const [initialValues] = useState<ILoginForm>({ login: '', password: '' });
 
   /**
    * Login user
@@ -39,21 +41,50 @@ const Login: SSRComponent<Props> = ({ authStore: { error, signIn, isLoading } })
         <title>{t('login-page:pageTitle')}</title>
       </Helmet>
       {(isLoading && <div>Loading...</div>) || (
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}>
-          <Form>
-            <label htmlFor="login">{t('login-page:fieldLogin')}</label>
-            <Field name="login" placeholder={t('login-page:fieldLogin')} />
-            <ErrorMessage name="login" />
-            <label htmlFor="password">{t('login-page:fieldPassword')}</label>
-            <Field type="password" name="password" placeholder={t('login-page:fieldPassword')} />
-            <ErrorMessage name="password" />
-            {error && <span>{error}</span>}
-            <SubmitButton>{t('login-page:buttonText')}</SubmitButton>
-          </Form>
-        </Formik>
+        <div className={combineCss('container', styles.container)}>
+          <div className={styles.wrapper}>
+            <div className={styles.head}>
+              <h1 className={styles.title}>{t('translation:coronaTitle')}</h1>
+              <p className={styles.description}>
+                <span className={styles.welcomeMessage}>{t('translation:welcomeMessage')}</span>
+                <span className={styles.text}>{t('translation:warningText')}</span>
+              </p>
+            </div>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmit}>
+              <Form className={styles.form} autoComplete="off">
+                <label htmlFor="login" className={styles.label}>
+                  <Field
+                    className={styles.input}
+                    type="text"
+                    name="login"
+                    autoComplete="new-password"
+                    placeholder={t('login-page:fieldLogin')}
+                  />
+                  <span className={styles.errorMessage}>
+                    <ErrorMessage name="login" />
+                  </span>
+                </label>
+                <label htmlFor="password" className={styles.label}>
+                  <Field
+                    className={styles.input}
+                    name="password"
+                    type="password"
+                    autoComplete="new-password"
+                    placeholder={t('login-page:fieldPassword')}
+                  />
+                  <span className={styles.errorMessage}>
+                    <ErrorMessage name="password" />
+                  </span>
+                </label>
+                <span className={combineCss(styles.errorMessage, styles.mainError)}>{error}</span>
+                <SubmitButton className={styles.button}>{t('login-page:buttonText')}</SubmitButton>
+              </Form>
+            </Formik>
+          </div>
+        </div>
       )}
     </>
   );
