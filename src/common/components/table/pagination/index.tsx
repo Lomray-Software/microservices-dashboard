f7407d34ea89e68a4c4287740a72af6365a37c88
Alexey <alexeidanilovich031193@gmail.com>
2022-09-21 14:05:24 +0400

    fix: fixed table pagination, sidebar menu layout (#12)

--- src/common/components/table/pagination/index.tsx
+++ src/common/components/table/pagination/index.tsx
@@ -6,6 +6,7 @@ import {
 } from '@mdi/js';
 import type { FC } from 'react';
 import React from 'react';
+import type { ReactPaginateProps } from 'react-paginate';
 import ReactPaginate from 'react-paginate';
 import Select from '../select';
 import Button from './button';
@@ -15,46 +16,61 @@ interface IPagination {
   page: number;
   size: number;
   count: number;
-  handlePagination: (selected: number) => void;
-  onStartPage: () => void;
-  onPreviousPage: () => void;
-  onLastPage: () => void;
-  onNextPage: () => void;
+  setPage: (selected: number) => void;
   onChangePageSize: (size: number) => void;
 }
 
-const Pagination: FC<IPagination> = ({
-  page,
-  size,
-  count,
-  handlePagination,
-  onStartPage,
-  onPreviousPage,
-  onLastPage,
-  onChangePageSize,
-  onNextPage,
-}) =>
-  count > 1 ? (
+const Pagination: FC<IPagination> = ({ page, size, count, setPage, onChangePageSize }) => {
+  const pageCount = Math.ceil(count / size);
+
+  const onStartPage = () => setPage(1);
+
+  const onLastPage = () => setPage(pageCount);
+
+  const onPreviousPage = () => setPage(page - 1);
+
+  const onNextPage = () => setPage(page + 1);
+
+  const onPageChange: ReactPaginateProps['onPageChange'] = ({ selected }) => setPage(selected + 1);
+
+  const isDisabledFirsts = page === 1;
+
+  const isDisabledLasts = page >= pageCount;
+
+  if (pageCount <= 1) {
+    return null;
+  }
+
+  return (
     <div className={styles.pagination}>
       <div className={styles.buttons}>
-        <Button iconPath={mdiChevronDoubleLeft} isDisabled={1 === page} onClick={onStartPage} />
-        <Button iconPath={mdiChevronLeft} isDisabled={1 === page} onClick={onPreviousPage} />
+        <Button
+          iconPath={mdiChevronDoubleLeft}
+          isDisabled={isDisabledFirsts}
+          onClick={onStartPage}
+        />
+        <Button iconPath={mdiChevronLeft} isDisabled={isDisabledFirsts} onClick={onPreviousPage} />
         <ReactPaginate
-          forcePage={page}
+          forcePage={page - 1}
           className={styles.itemPagination}
           nextLabel={null}
           previousLabel={null}
           breakLabel="..."
-          pageRangeDisplayed={size}
-          pageCount={count}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
           activeClassName={styles.itemPaginationActive}
-          onPageChange={({ selected }) => handlePagination(selected)}
+          onPageChange={onPageChange}
         />
-        <Button iconPath={mdiChevronRight} isDisabled={count <= page} onClick={onNextPage} />
-        <Button iconPath={mdiChevronDoubleRight} isDisabled={count <= page} onClick={onLastPage} />
+        <Button iconPath={mdiChevronRight} isDisabled={isDisabledLasts} onClick={onNextPage} />
+        <Button
+          iconPath={mdiChevronDoubleRight}
+          isDisabled={isDisabledLasts}
+          onClick={onLastPage}
+        />
       </div>
       <Select pageSize={size} setPageSize={onChangePageSize} />
     </div>
-  ) : null;
+  );
+};
 
-export default Pagination;
+export default React.memo(Pagination);
