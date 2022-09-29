61d8cac3f7572f12935376bdac0d2373146b4c49
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-09-29 19:57:05 +0200

    fix: refresh user once for spa

--- src/common/components/layouts/user/index.tsx
+++ src/common/components/layouts/user/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Footer from '@components/footer';
 import Header from '@components/header/index.wrapper';
 import SideMenu from '@components/side-menu/index.wrapper';
-import { IS_PROD } from '@constants/index';
+import { IS_CLIENT, IS_PROD, IS_SERVER } from '@constants/index';
 import InitialProps from '@helpers/initial-props';
 import type { SSRComponent } from '@interfaces/ssr-component';
 import ApiClient from '@services/api-client';
@@ -24,12 +24,21 @@ const User: SSRComponent<Props> = () => (
   </div>
 );
 
+let hasUserRefreshed = false;
+
 User.getInitialProps = InitialProps(async ({ userStore }, ctx) => {
   if (
-    ctx.req?.universalCookies?.get('jwt-access') ||
-    (IS_PROD && ApiClient.getRefreshTokenPayload()?.userId)
+    // SSR
+    (IS_SERVER && ctx.req?.universalCookies?.get('jwt-access')) ||
+    // SPA
+    (IS_CLIENT && IS_PROD && ApiClient.getRefreshTokenPayload()?.userId)
   ) {
+    if (hasUserRefreshed) {
+      return;
+    }
+
     await userStore.refresh();
+    hasUserRefreshed = true;
   }
 }, stores) as never;
 
